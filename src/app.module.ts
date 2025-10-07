1a58a1ec614c19323e3966d9404c9a88583772b1
Alexis Dorado Muñoz <alexis.dorado.munoz@gmail.com>
2025-10-07 14:42:21 -0500

    Conectar aplicación a la base de datos
    * Se instalan y configuran las dependencias necesarias para la base de datos (@nestjs/typeorm, typeorm, pg) y el manejo de entorno (@nestjs/config).
    * Se configura AppModule para cargar variables de entorno globalmente y establecer la conexión con la base de datos PostgreSQL usando dichos valores.
    * Se actualiza el archivo README.md con instrucciones detalladas para la configuración y ejecución del proyecto.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      // Configuración de la base de datos usando variables de entorno
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT ?? 5432),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: process.env.ENV === 'development',
+    }),
+  ],
   controllers: [],
   providers: [],
 })
